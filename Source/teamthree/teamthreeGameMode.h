7c0d34bdf3af6ff4f2e8a18a417cc182365fb2ad
James Young <jamescallumyoung@gmail.com>
2015-11-04 18:48:59 +0000

    ue4 init commit

---
+++ Source/teamthree/teamthreeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "teamthreeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMTHREE_API AteamthreeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
